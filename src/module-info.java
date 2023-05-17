cb12b8e5485f0a2df7bc90475b4e88ceac27fcbc
Fornalik Loline <loline.fornalik@iut-rodez.fr>
2023-05-17 16:14:49 +0200

    commit initial

---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+/*
+ * module-info.java                                      17 mai 2023
+ * IUT Rodez, info1 2022-2023, pas de copyright ni "copyleft" 
+ */
+/**
+ * @author loline.fornalik
+ *
+ */
+module TP4 {
+}
